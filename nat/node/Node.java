d3c4535108a0520e18e99598f5b70ea959655c28
王正陽 <wzy9607@mail.ustc.edu.cn>
2016-05-02 08:37:50 +0000

    Upload new file

---
+++ nat/node/Node.java
@@ -0,0 +1,110 @@
+import java.io.File;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.locks.ReentrantLock;
+
+import com.barchart.udt.ExceptionUDT;
+import com.barchart.udt.SocketUDT;
+import com.barchart.udt.TypeUDT;
+import com.barchart.udt.net.NetInputStreamUDT;
+import com.barchart.udt.net.NetOutputStreamUDT;
+
+/**
+ * 
+ */
+
+/**
+ * @author wzy
+ *
+ */
+public class Node {
+
+	protected String ID;
+	protected Set<String> nodeIDs;
+	protected Thread node_thread;
+
+	protected InetAddress IP_local;
+	protected InetAddress IP_local_nat;
+	protected int Port_local_nat;
+
+	protected Queue<String> node_inserted_lm;
+	protected Queue<String> node_deleted_lm;
+	protected Thread link_maintainer_thread;
+
+	protected Map<String, SocketUDT> links_p;
+	protected Map<String, Thread> links_p_t;
+
+	protected SocketUDT server;
+	protected String server_host;
+	protected int server_port;
+	protected InputStream in_s;
+	protected OutputStream out_s;
+	protected ReentrantLock server_link_lock;
+	protected Thread server_link_thread;
+	protected Map<String, Queue<Map<String, String>>> messages_from_server;
+
+	/**
+	 * @param server_host
+	 * @param server_port
+	 */
+	public Node(String server_host, int server_port) {
+		nodeIDs = ConcurrentHashMap.<String> newKeySet();
+		node_inserted_lm = new ConcurrentLinkedQueue<>();
+		node_deleted_lm = new ConcurrentLinkedQueue<>();
+		links_p = new ConcurrentHashMap<>();
+		links_p_t = new ConcurrentHashMap<>();
+		messages_from_server = new ConcurrentHashMap<>();
+		messages_from_server.put("Node", new ConcurrentLinkedQueue<Map<String, String>>());
+		messages_from_server.put("Link", new ConcurrentLinkedQueue<Map<String, String>>());
+		this.server_host = server_host;
+		this.server_port = server_port;
+		try {
+			IP_local = InetAddress.getLocalHost();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			IP_local = null;
+			e.printStackTrace();
+		}
+		byte arr[] = new byte[1024];
+		try {
+			server = new SocketUDT(TypeUDT.STREAM);
+			server.setBlocking(true);
+			server.connect(new InetSocketAddress(server_host, server_port));
+			server.setSoTimeout(10000);
+			IP_local_nat = server.getLocalInetAddress();
+			Port_local_nat = server.getLocalInetPort();
+			in_s = new NetInputStreamUDT(server);// currently not in use
+			out_s = new NetOutputStreamUDT(server);// currently not in use
+			server.receive(arr);
+			// ID = ;
+		} catch (ExceptionUDT e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		// TODO receive NodeID table
+		link_maintainer_thread = new Thread(new LinkMaintainer(this));
+		link_maintainer_thread.start();
+		node_thread = new Thread(new NodeThread(this));
+		node_thread.start();
+		server_link_thread = new Thread(new ServerLink(this));
+		server_link_thread.start();
+	}
+
+	/**
+	 * @param file
+	 * @param ID_p
+	 * @return
+	 */
+	public boolean transfer_file(File file, String ID_p) {
+		return false;
+
+	}
+}
