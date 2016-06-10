fc99d0e709408d9b9af0b29d99d8cc36f8b99e6a
Xiao Su <sx951018@mail.ustc.edu.cn>
2016-06-10 09:36:11 +0000

    Update MultiServer.java

--- src/nat/server/MultiServer.java
+++ src/nat/server/MultiServer.java
@@ -140,6 +140,7 @@ class MultiServerImplementation implements Runnable
         try 
         {
             temp_clientNum = this.clientNum++;
+            ++this.mapVertices;
         }
         finally 
         {
@@ -395,271 +396,334 @@ class MultiServerImplementation implements Runnable
         sock.close();
     }
 
-    public void config_map(Map<String, String> info, SocketUDT sock)
-    {
+        public void map_change(Map<String, String> info, SocketUDT sock) {
         int ID = Integer.parseInt(info.get("ID"));
         double RTT, bandwidth, lostRate, weight;
-        try 
-        {
+        try {
             lock.lock();
-            ++this.mapVertices;
-            for(int i = 0; i < Integer.parseInt(info.get("cnt")); ++i)
-            {
-                if(info.get("RTT_" + i).trim().equals("Infinity"))
+            //++this.mapVertices;//加到register处,掉线时要--，不知道为什么把check注释掉了所以先不管
+            for (int i = 0; i < Integer.parseInt(info.get("cnt")); ++i) {
+                if (info.get("RTT_" + i).trim().equals("Infinity")) {
                     continue;
+                }
                 RTT = Double.parseDouble(info.get("RTT_" + i));
                 bandwidth = Double.parseDouble(info.get("bandwidth_" + i));
                 lostRate = Double.parseDouble(info.get("lostRate_" + i));
-                weight = 1.0 * RTT + 0.2 * lostRate + 1.0 * bandwidth;
-                this.fi.weight[ID][i] = this.fi.weight[i][ID] = weight;
+                weight = 1.0 * RTT + 0.2 * lostRate + 1.0 * bandwidth;//比例问题，因为这三项的单位不同，或许应该给每项设定一个标准值，然后用其和标准值的比来表示weight
+                if (this.fi.weight[ID][i] == INFINITY && this.fi.weight[i][ID] == INFINITY) {
+                    this.fi.weight[ID][i] = this.fi.weight[i][ID] = weight;
+                } else {
+                    this.fi.weight[ID][i] = this.fi.weight[i][ID] = (this.fi.weight[ID][i] + weight) / 2;
+                    //有问题，更新的时候，如果要平均应该是和新到的平均，应该不能和原来的数据平均，现在这样写的话，更新时先到的会和原来的取平均。而如果要控制它只和
+                    //另外一边更新的取平均(也就是让先到的更新包直接取代后到的，后到的则和先到的平均)，这样的话似乎比较难写，所以在想要不要就不取平均了...
+                }
             }
-            this.fi.init();
+            this.fi.res_init();
+            this.fi.path_init();
             this.fi.floyd();
-        } 
-        catch (Exception e) 
-        {
-        
-        }
-        finally
-        {
+        } catch (Exception e) {
+
+        } finally {
             lock.unlock();
         }
     }
-    
-    public void return_route(Map<String, String> info, SocketUDT sock)
-    {
-        List<Integer> best = new ArrayList<>();
-        this.fi.output_toList(Integer.parseInt(info.get("ID")), Integer.parseInt(info.get("ID_target")), best);
-        Map<String, String> sendBack;
-        sendBack = new HashMap<>();
-        if(this.fi.res[Integer.parseInt(info.get("ID"))][Integer.parseInt(info.get("ID_target"))] == this.INFINITY)
-        {
-            //发送错误包，无法连接
+
+    public void map_edge_insert(Map<String, String> info, SocketUDT sock) {
+        int from = Integer.parseInt(info.get("ID"));
+        int to = Integer.parseInt(info.get("to"));
+        double RTT, bandwidth, lostRate, weight;
+        try {
+            lock.lock();
+            RTT = Double.parseDouble(info.get("RTT_"));
+            bandwidth = Double.parseDouble(info.get("bandwidth_"));
+            lostRate = Double.parseDouble(info.get("lostRate_"));
+            weight = 1.0 * RTT + 0.2 * lostRate + 1.0 * bandwidth;
+            if (this.fi.weight[from][to] == INFINITY && this.fi.weight[to][from] == INFINITY) {
+                this.fi.weight[from][to] = this.fi.weight[to][from] = weight;
+            } else {
+                this.fi.weight[from][to] = this.fi.weight[to][from] = (this.fi.weight[from][to] + weight) / 2;
+            }
+
+            this.fi.res_init();
+            this.fi.path_init();
+            this.fi.floyd();
+        } catch (Exception e) {
+
+        } finally {
+            lock.unlock();
         }
-        else
-        {
+    }
+
+    public void map_edge_delete(Map<String, String> info, SocketUDT sock) {
+        int from = Integer.parseInt(info.get("ID_source"));
+        int to = Integer.parseInt(info.get("ID"));
+        //double RTT, bandwidth, lostRate, weight;
+        try {
+            lock.lock();
+            this.fi.weight[from][to] = this.fi.weight[to][from] = INFINITY;
+            this.fi.res_init();
+            this.fi.path_init();
+            this.fi.floyd();
+        } catch (Exception e) {
+
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void return_route(Map<String, String> info, SocketUDT sock) {
+        List<Integer> best ;
+        List<List<Integer>> route_list =new ArrayList<>();
+        int from = Integer.parseInt(info.get("ID"));
+        int to = Integer.parseInt(info.get("ID_target"));
+        //未完待续
+        int [] able=new int[mapVertices];
+        int count = 0;
+        for (int i = 0; i < mapVertices; i++) {
+            if (fi.weight[from][i] > 0 || fi.weight[from][i] != INFINITY) {
+                able[count++]=i;//统计与from直接相连的node数
+            }
+        }
+        int[] a = new int[count];//用一个类似bool数组来表示是否算过，0代表算过
+
+        double min = INFINITY;
+        int temp=(int)INFINITY;
+        for (int i = 0; i < count; i++) {
+            best = new ArrayList<>();
+            for (int j = 0; j < count; j++) {
+                if (a[j]!=0) {
+                    if(fi.weight[from][able[j]]+fi.res[able[j]][to]<min)
+                        min=fi.weight[from][able[j]]+fi.res[able[j]][to];
+                        temp=able[j];
+                        a[j]=1;
+                }
+            }
+            best.add(from);
+            this.fi.output_toList(temp,to,best);
+            route_list.add(best);
+            }//得到排好序的route_list,最短的在最前面,未完待续        
+                 
+       //this.fi.output_toList(Integer.parseInt(info.get("ID")), Integer.parseInt(info.get("ID_target")), best);
+       // Map<String, String> sendBack;
+        //sendBack = new HashMap<>();
+        if (this.fi.res[Integer.parseInt(info.get("ID"))][Integer.parseInt(info.get("ID_target"))] == this.INFINITY) {
+            //发送错误包，无法连接
+        } else {
             //发送路径
         }
     }
-    
-    public void record_info(Map<String, String> info, SocketUDT sock) throws ExceptionUDT
-    {
+
+    public void record_info(Map<String, String> info, SocketUDT sock) throws ExceptionUDT {
         String connectivity = info.get("Connectivity");
         String IP_from = null, port_from = null;
-        try
-        {
+        try {
             IP_from = sock.getRemoteInetAddress().toString().split("/")[1];
             port_from = sock.getRemoteInetPort() + "";
-        }
-        catch(NullPointerException e)
-        {
+        } catch (NullPointerException e) {
             log("sock null");
-        }     
+        }
         String destination = info.get("ID");
         String ID_source = info.get("ID_source");
         Client client_from = find_client(ID_source);
         Client client_to = find_client(destination);
         List<Client> connect_list;
         Map.Entry<Client, Client> pair_from;
-        if(client_from == null || client_to == null)
-        {
+        if (client_from == null || client_to == null) {
             log("Not online...");
-        }
-        else
-        {
+        } else {
             pair_from = new AbstractMap.SimpleEntry<>(client_from, client_to);
-            if("true".equals(connectivity.trim()))
-            {
+            if ("true".equals(connectivity.trim())) {
                 connect_list = route_table.get(client_from);
-                if(connect_list == null)
-                {
-                    connect_list=new ArrayList<>();
+                if (connect_list == null) {
+                    connect_list = new ArrayList<>();
                     connect_list.add(client_to);
                     route_table.put(client_from, connect_list);
-                }
-                else
-                {
-                    if(connect_list.indexOf(client_to) == -1)
-                        connect_list.add(client_to);
+                } else if (connect_list.indexOf(client_to) == -1) {
+                    connect_list.add(client_to);
                 }//add B to A's connectlist
                 connect_list = route_table.get(client_to);
-                if(connect_list == null)
-                {
-                    connect_list=new ArrayList<>();
+                if (connect_list == null) {
+                    connect_list = new ArrayList<>();
                     connect_list.add(client_from);
-                    route_table.put(client_to, connect_list);               
+                    route_table.put(client_to, connect_list);
+                } else if (connect_list.indexOf(client_from) == -1) {
+                    connect_list.add(client_from);
                 }
-                else
-                {
-                    if(connect_list.indexOf(client_from) == -1)
-                        connect_list.add(client_from);
-                }
-                record.put(pair_from,"true");
+                record.put(pair_from, "true");
             }//if connectivity=true
-            else
-            {
-                record.put(pair_from,"false");
+            else {
+                record.put(pair_from, "false");
+                map_edge_delete(info, sock);//收到"LinkC false直接delete边"
             }//do nothing?
             client_from.isConnecting = false;
             client_to.isConnecting = false;
-        }   
+        }
     }
-    
+
     @Override
-    public void run()
-    {
+    public void run() {
         SocketUDT sock;
-        if(!this.check_on)  //让一个线程用来检测定时在线
+        if (!this.check_on) //让一个线程用来检测定时在线
         {
             lock.lock();
-            try
-            {
+            try {
                 this.check_on = true;
-            }
-            finally
-            {
+            } finally {
                 lock.unlock();
             }
-            try 
-            {
+            try {
                 check_online();
-            } 
-            catch (InterruptedException | ExceptionUDT ex) 
-            {
+            } catch (InterruptedException | ExceptionUDT ex) {
                 Logger.getLogger(MultiServerImplementation.class.getName()).log(Level.SEVERE, null, ex);
-            } 
-            catch (IOException ex) 
-            {
+            } catch (IOException ex) {
                 Logger.getLogger(MultiServerImplementation.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
-        while(true)
-        {
-            try 
-            {
+        while (true) {
+            try {
                 sock = this.serverSocket.accept();
                 dual_with(sock);
-            } 
-            catch (ExceptionUDT ex) 
-            {
+            } catch (ExceptionUDT ex) {
                 Logger.getLogger(MultiServer.class.getName()).log(Level.SEVERE, null, ex);
             }
-            
+
         }
     }
-    
-    private void log(String message)
-    {
+
+    private void log(String message) {
         System.out.println(message);
     }
-    
+
 }
 
-class Client
-{
+class Client {
+
     String ID = null;
     String userName = null;
     String IP_maintain = null;
     String port_maintain = null;
     SocketUDT link_maintain;
     boolean isConnecting = false;
-    public Client(String userName, long clientNum, InetAddress IP_maintain, int port_maintain, SocketUDT link_maintain)
-    {
+
+    public Client(String userName, long clientNum, InetAddress IP_maintain, int port_maintain, SocketUDT link_maintain) {
         this.userName = userName;
         this.ID = String.format("%05d", clientNum);
         this.IP_maintain = IP_maintain.toString().split("/")[1];
         this.port_maintain = port_maintain + "";
         this.link_maintain = link_maintain;
     }
+
     @Override
-    public String toString()
-    {
+    public String toString() {
         return this.userName + "\n" + this.ID + "\n" + this.IP_maintain + "\n" + this.port_maintain;
     }
 }
 
-class Floyd_implementation
-{
-    public double [][] res;
-    public double [][] weight;
-    public int [][] path_temp;
+class Floyd_implementation {
+
+    public double[][] res;
+    public double[][] weight;
     double INF = 66666666;
-    public Floyd_implementation(int size)
-    {
+    public int[][] path_temp;
+
+    public Floyd_implementation(int size) {
         this.res = new double[size][size];
         this.weight = new double[size][size];
-        for(int i = 0; i < size; ++i)
-            for (int j = 0; j < size; j++)
-                this.res[i][j] = this.weight[i][j] = INF;
         this.path_temp = new int[size][size];
+        for (int i = 0; i < size; ++i) {
+            for (int j = 0; j < size; ++j) {
+                this.res[i][j] = this.weight[i][j] = INF;
+            }
+        }
+
     }
-    
-    public void init()
-    {
-        for(int i = 0; i < res.length; ++i)
-            for(int j = 0; j < res.length; ++j)
+
+    public void res_init() {
+        for (int i = 0; i < res.length; ++i) {
+            for (int j = 0; j < res.length; ++j) {
                 res[i][j] = weight[i][j];
+            }
+        }
     }
-    
-    public void floyd()
-    {
+
+    public void weight_init(double[][] w) {
+        for (int i = 0; i < weight.length; ++i) {
+            for (int j = 0; j < weight.length; ++j) {
+                weight[i][j] = w[i][j];
+            }
+        }
+    }
+
+    public void path_init() {
+        for (int i = 0; i < path_temp.length; ++i) {
+            for (int j = 0; j < path_temp.length; ++j) {
+                if (weight[i][j] == INF) {
+                    path_temp[i][j] = (int) INF;
+                } else {
+                    path_temp[i][j] = j;
+                }
+            }
+        }
+
+    }
+
+    public void floyd() {
         int i, j, k;
-        for(k = 0; k < this.res.length; ++k)
-            for(i = 0; i < this.res.length; ++i)
-                for(j = 0; j < this.res.length; ++j)
-                    if(this.res[i][k] + this.res[k][j] < this.res[i][j])
-                    {
+        for (k = 0; k < this.res.length; ++k) {
+            for (i = 0; i < this.res.length; ++i) {
+                for (j = 0; j < this.res.length; ++j) {
+                    if (this.res[i][k] + this.res[k][j] < this.res[i][j]) {
                         this.res[i][j] = this.res[i][k] + this.res[k][j];
-                        this.path_temp[i][j] = k;
+                        this.path_temp[i][j] = k;//path_temp存的是i到j的第一个中转点
                     }
+                }
+            }
+        }
     }
-    
-    public void output_toList(int i, int j, List<Integer> res)
-    {
+
+    public void output_toList(int i, int j, List<Integer> res) {
         res.add(i);
         output_toList_implement(i, j, res);
     }
-    
-    private int output_toList_implement(int i, int j, List<Integer> res)
-    {
-        if(i == j)
+
+    private int output_toList_implement(int i, int j, List<Integer> res) {
+        if (i == j) {
             return i;
-        if(this.path_temp[i][j] == 0)
-        {   
+        }
+        if (this.path_temp[i][j] == j)//代表不通过中转
+        {
             res.add(j);
             return j;
-        }
-        else
-        {
-            output_toList_implement(i, path_temp[i][j], res);
+        } else {
+            output_toList_implement(i, path_temp[i][j], res);//否则递归
             output_toList_implement(path_temp[i][j], j, res);
         }
         return 0;
     }
-    
-    public void output(int i, int j)
-    {
-        if(i == j)return;
-        if(this.path_temp[i][j] == 0)
-            System.out.print(j + " ");
-        else
-        {
+
+    public void output(int i, int j) {
+        if (i == j) {
+            return;
+        }
+        if (this.path_temp[i][j] == j) {
+            System.out.print(j);
+        } else {
             output(i, path_temp[i][j]);
+            System.out.print("->");
             output(path_temp[i][j], j);
         }
     }
-    
-    public void count()
-    {
-        for(int i = 0; i < this.res.length; ++i)
-            for(int j = 0; j < this.res.length; ++j)
-                if(this.path_temp[i][j] == INF || i == j)
-                    ;
-                else
-                {
-                    System.out.print(i + " ");
+
+    public void count() {
+        for (int i = 0; i < this.res.length; ++i) {
+            for (int j = 0; j < this.res.length; ++j) {
+                if (this.path_temp[i][j] == INF || i == j)
+                    ; else {
+                    System.out.print(i + "->");
                     output(i, j);
                     System.out.println();
                 }
+            }
+        }
     }
 }
