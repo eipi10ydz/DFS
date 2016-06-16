c7c21582eaeef5e0138fa548f9418d0e40097175
王正陽 <wzy9607@mail.ustc.edu.cn>
2016-06-16 06:28:50 +0000

    No longer maintain any link & add direct connect method.

---
+++ src/nat/node/LinkEstablisherThreadS2.java
@@ -0,0 +1,50 @@
+package data_transferor;
+
+import java.util.Map;
+
+import com.barchart.udt.ExceptionUDT;
+
+class LinkEstablisherThreadS2 implements Runnable {
+
+	private Node node;
+
+	/**
+	 * @param node
+	 */
+	public LinkEstablisherThreadS2(Node node) {
+		this.node = node;
+	}
+
+	@Override
+	public void run() {
+		try {
+			while (!Thread.currentThread().isInterrupted()) {
+				// check if other Nodes try to establish links with this
+				while (!node.messages_from_server.get("Link").isEmpty()) {
+					Map<String, String> pac = node.messages_from_server.get("Link").poll();
+					if (pac.get("type").equals("LinkE") && pac.get("type_d").equals("03")) {
+						try {
+							if (node.link_establisher.establish_link_s2(pac.get("ID"), pac.get("IP"),
+									Integer.parseInt(pac.get("Port")))) {
+								node.link_timers.remove(pac.get("ID"));
+							}
+						} catch (ExceptionUDT e) {
+							e.printStackTrace();
+						} catch (NodeException e) {
+							e.printStackTrace();
+						} catch (PackException e) {
+							e.printStackTrace();
+						}
+					} else {
+						// TODO Something wrong
+					}
+				}
+				Thread.sleep(1000);
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		return ;
+	}
+
+}
