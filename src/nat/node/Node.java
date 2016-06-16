0b6d37ce20483a5b85409d66b742c11baf4d6293
王正陽 <wzy9607@mail.ustc.edu.cn>
2016-06-16 06:29:14 +0000

    No longer maintain any link.

--- src/nat/node/Node.java
+++ src/nat/node/Node.java
@@ -42,14 +42,17 @@ public class Node {
 	protected Thread node_thread;
 
 	protected InetAddress IP_local;
+	protected Map<String, String> node_IPs;
 
 	protected Queue<String> node_inserted_lm;
 	protected Queue<String> node_deleted_lm;
-	protected Thread link_maintainer_thread;
 
-	protected Map<String, SocketUDT> links_p;
-	protected Map<String, Thread> links_p_t;
-	protected Map<String, ReentrantLock> links_p_l;
+	protected Thread link_establisher_thread_m;
+	protected Thread link_establisher_thread_s1;
+	protected Thread link_establisher_thread_s2;
+	protected LinkEstablisher link_establisher;
+	protected Map<String, ReentrantLock> link_locks;
+	protected Map<String, Timer> link_timers;
 
 	protected NodeRouter router;
 	protected Queue<String> node_inserted_rout;
@@ -61,11 +64,10 @@ public class Node {
 	protected int server_port;
 	protected InputStream in_s;
 	protected OutputStream out_s;
-	protected ReentrantLock server_link_lock;
+	// protected SocketUDT server;
 	protected Thread server_link_thread;
 	protected Map<String, Queue<Map<String, String>>> messages_from_server;
 
-	protected Map<String, DataTransferor> data_arrived;
 	protected ExecutorService data_to_send;
 
 	/**
@@ -74,16 +76,19 @@ public class Node {
 	 * @param server_port
 	 * @throws ExceptionUDT
 	 * @throws NodeException
+	 * @throws PackException
 	 */
-	public Node(String user_name, String server_host, int server_port) throws ExceptionUDT, NodeException {
+	public Node(String user_name, String server_host, int server_port)
+			throws ExceptionUDT, PackException, NodeException {
 		this.user_name = user_name;
 		nodeIDs = ConcurrentHashMap.<String> newKeySet();
 		UName_ID = new ConcurrentHashMap<>();
+		node_IPs = new ConcurrentHashMap<>();
 		node_inserted_lm = new ConcurrentLinkedQueue<>();
 		node_deleted_lm = new ConcurrentLinkedQueue<>();
-		links_p = new ConcurrentHashMap<>();
-		links_p_t = new ConcurrentHashMap<>();
-		links_p_l = new ConcurrentHashMap<>();
+		link_establisher = new LinkEstablisher(this);
+		link_locks = new ConcurrentHashMap<>();
+		link_timers = new ConcurrentHashMap<>();
 		router = new NodeRouter(this);
 		node_inserted_rout = new ConcurrentLinkedQueue<>();
 		node_deleted_rout = new ConcurrentLinkedQueue<>();
@@ -93,8 +98,6 @@ public class Node {
 		messages_from_server.put("ERR", new ConcurrentLinkedQueue<Map<String, String>>());
 		this.server_host = server_host;
 		this.server_port = server_port;
-		server_link_lock = new ReentrantLock();
-		data_arrived = new ConcurrentHashMap<>();
 		data_to_send = Executors.newCachedThreadPool();
 		try {
 			IP_local = InetAddress.getLocalHost();
@@ -116,7 +119,7 @@ public class Node {
 		pac.put("Insertion", "Insertion");
 		try {
 			str = Packer.pack("NodeI", "00", pac);
-		} catch (NodeException e) {// just used for debug
+		} catch (PackException e) {// just used for debug
 			e.printStackTrace();
 		}
 		server.send(str.getBytes(Charset.forName("ISO-8859-1")));// Insertion
@@ -127,9 +130,10 @@ public class Node {
 		ID = pac.get("ID");
 		pac = new ConcurrentHashMap<String, String>();
 		pac.put("UName", this.user_name);
+		pac.put("LIP", this.IP_local.getHostAddress());
 		try {
 			str = Packer.pack("NodeI", "03", pac);
-		} catch (NodeException e) {// just used for debug
+		} catch (PackException e) {// just used for debug
 			e.printStackTrace();
 		}
 		server.send(str.getBytes(Charset.forName("ISO-8859-1")));// send user
@@ -139,13 +143,19 @@ public class Node {
 		pac = Packer.Check_table(str);
 		int cnt = Integer.parseInt(pac.get("cnt"));
 		String s1 = new String("UName_");
+		String s3 = new String("LIP_");
 		String s2 = new String("ID_");
 		for (int i = 1; i <= cnt; i++) {
 			nodeIDs.add(pac.get(s2 + i));
 			UName_ID.put(pac.get(s1 + i), pac.get(s2 + i));
+			node_IPs.put(pac.get(s2 + i), pac.get(s3 + i));
 		}
-		link_maintainer_thread = new Thread(new LinkMaintainer(this));
-		link_maintainer_thread.start();
+		link_establisher_thread_m = new Thread(new LinkEstablisherThreadM(this));
+		link_establisher_thread_m.start();
+		link_establisher_thread_s1 = new Thread(new LinkEstablisherThreadS1(this));
+		link_establisher_thread_s1.start();
+		link_establisher_thread_s2 = new Thread(new LinkEstablisherThreadS2(this));
+		link_establisher_thread_s2.start();
 		node_thread = new Thread(new NodeThread(this));
 		node_thread.start();
 		server_link_thread = new Thread(new ServerLink(this));
@@ -167,18 +177,14 @@ public class Node {
 	 * @param ID_p
 	 * @return result
 	 */
-	public Future<Boolean> transfer_message(String str, String ID_p) {
-		if (str == null || str.isEmpty()) {
-			throw new IllegalArgumentException("Empty String.");
-		}
-		if (nodeIDs.contains(ID_p)) {// Launch a new task and hold its result
-			Future<Boolean> result;
-			result = data_to_send.submit(new DataSender(this, ID_p, str));
-			return result;
-		} else {
-			throw new IllegalArgumentException("The destination isn't exist.");
-		}
-	}
+	/*
+	 * public Future<Boolean> transfer_message(String str, String ID_p) { if
+	 * (str == null || str.isEmpty()) { throw new IllegalArgumentException(
+	 * "Empty String."); } if (nodeIDs.contains(ID_p)) {// Launch a new task and
+	 * hold its result Future<Boolean> result; result = data_to_send.submit(new
+	 * DataSender(this, ID_p, str)); return result; } else { throw new
+	 * IllegalArgumentException("The destination isn't exist."); } }
+	 */
 
 	/**
 	 * @param obj
