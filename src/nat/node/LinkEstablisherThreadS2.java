dc461c96d59a4cf7540078de58c473e447b7b360
王正陽 <wzy9607@mail.ustc.edu.cn>
2016-06-17 03:20:08 +0000

    Add direct connect from the peer.

--- src/nat/node/LinkEstablisherThreadS2.java
+++ src/nat/node/LinkEstablisherThreadS2.java
@@ -22,18 +22,30 @@ class LinkEstablisherThreadS2 implements Runnable {
 				// check if other Nodes try to establish links with this
 				while (!node.messages_from_server.get("Link").isEmpty()) {
 					Map<String, String> pac = node.messages_from_server.get("Link").poll();
-					if (pac.get("type").equals("LinkE") && pac.get("type_d").equals("03")) {
-						try {
-							if (node.link_establisher.establish_link_s2(pac.get("ID"), pac.get("IP"),
-									Integer.parseInt(pac.get("Port")))) {
-								node.link_timers.remove(pac.get("ID"));
+					if (pac.get("type").equals("LinkE")) {
+						if (pac.get("type_d").equals("03")) {
+							try {
+								if (node.link_establisher.establish_link_s2(pac.get("ID"), pac.get("IP"),
+										Integer.parseInt(pac.get("Port")))) {
+									node.link_timers.remove(pac.get("ID"));
+								}
+							} catch (ExceptionUDT e) {
+								e.printStackTrace();
+							} catch (NodeException e) {
+								e.printStackTrace();
+							} catch (PackException e) {
+								e.printStackTrace();
 							}
-						} catch (ExceptionUDT e) {
-							e.printStackTrace();
-						} catch (NodeException e) {
-							e.printStackTrace();
-						} catch (PackException e) {
-							e.printStackTrace();
+						} else if (pac.get("type_d").equals("07")) {
+							try {
+								if (node.link_establisher.establish_link_s3(pac.get("ID"))) {
+									node.link_timers.remove(pac.get("ID"));
+								}
+							} catch (ExceptionUDT e) {
+								e.printStackTrace();
+							}
+						} else {
+							// TODO Something wrong
 						}
 					} else {
 						// TODO Something wrong
@@ -44,7 +56,7 @@ class LinkEstablisherThreadS2 implements Runnable {
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		return ;
+		return;
 	}
 
 }
