710406bdff524489c4a4bf1ba73db0ef8a2f09d7
王正陽 <wzy9607@mail.ustc.edu.cn>
2016-06-18 03:28:45 +0000

    Replace LinkConnectivityChecker.java

--- src/nat/node/LinkConnectivityChecker.java
+++ src/nat/node/LinkConnectivityChecker.java
@@ -1,4 +1,4 @@
-package nodetest;
+package data_transferor;
 
 import java.nio.charset.Charset;
 import java.util.Map;
@@ -36,58 +36,42 @@ class LinkConnectivityChecker implements Runnable {
 		establish_links();
 		while (!Thread.currentThread().isInterrupted()) {
 			// check link timers
-			try {
-				for (Map.Entry<String, Timer> entry : node.link_timers.entrySet()) {
-					String nodeID = entry.getKey();
-					Timer timer = entry.getValue();
-					if (!node.nodeIDs.contains(nodeID)) {// the peer node has
-															// dropped
-						node.link_timers.remove(nodeID);
-					} else if (timer.isExpired()) {
-						try {
-							SocketUDT sock = node.link_establisher.establish_link_m(nodeID);
-							if (sock == null) {
-								timer.postpone(20000 * timer.getCnt());
-								timer.start();
-							} else {
-								node.link_timers.remove(nodeID);
-								String str = new String();
-								Map<String, String> pac = new ConcurrentHashMap<String, String>();
-								pac.put("Cardiopalmus", "Cardiopalmus");
-								pac.put("Palpitation", "Palpitation");
-								try {
-									str = Packer.pack("HEARTBEAT", pac);
-								} catch (PackException e) {// just used for
-															// debug
-									e.printStackTrace();
-								}
-								sock.send(str.getBytes(Charset.forName("ISO-8859-1")));// send
-																						// packet
-																						// HEARTBEAT
-								try {
-									sock.close();
-								} catch (Exception e) {
-									e.printStackTrace();
-								}
+			node.link_timers.forEach((nodeID, timer) -> {
+				if (!node.nodeIDs.contains(nodeID)) {// the peer node has
+														// dropped
+					node.link_timers.remove(nodeID);
+				} else if (timer.isExpired()) {
+					try {
+						SocketUDT sock = node.link_establisher.establish_link_m(nodeID);
+						if (sock == null) {
+							timer.postpone(20000 * timer.getCnt());
+							timer.start();
+						} else {
+							node.link_timers.remove(nodeID);
+							String str = new String();
+							Map<String, String> pac = new ConcurrentHashMap<String, String>();
+							pac.put("Cardiopalmus", "Cardiopalmus");
+							pac.put("Palpitation", "Palpitation");
+							try {
+								str = Packer.pack("HEARTBEAT", pac);
+							} catch (PackException e) {// just used for debug
+								e.printStackTrace();
 							}
-						} catch (PackException e) {
-							timer.postpone(20000 * timer.getCnt());
-							timer.start();
-							e.printStackTrace();
-						} catch (ExceptionUDT e) {
-							timer.postpone(20000 * timer.getCnt());
-							timer.start();
-							e.printStackTrace();
-						} catch (NodeException e) {
-							timer.postpone(20000 * timer.getCnt());
-							timer.start();
-							throw e;
+							sock.send(str.getBytes(Charset.forName("ISO-8859-1")));// send
+																					// packet
+																					// HEARTBEAT
+							try {
+								sock.close();
+							} catch (Exception e) {
+							}
 						}
+					} catch (PackException | ExceptionUDT | NodeException e) {
+						timer.postpone(20000 * timer.getCnt());
+						timer.start();
+						e.printStackTrace();
 					}
 				}
-			} catch (NodeException e) {
-				e.printStackTrace();
-			}
+			});
 			// check if new node inserted
 			while (!node.node_inserted_lm.isEmpty())
 				new_link_timer(node.node_inserted_lm.poll());
@@ -124,16 +108,9 @@ class LinkConnectivityChecker implements Runnable {
 					try {
 						sock.close();
 					} catch (Exception e) {
-						e.printStackTrace();
 					}
 				}
-			} catch (PackException e) {
-				new_link_timer(nodeID);
-				e.printStackTrace();
-			} catch (ExceptionUDT e) {
-				new_link_timer(nodeID);
-				e.printStackTrace();
-			} catch (NodeException e) {
+			} catch (PackException | ExceptionUDT | NodeException e) {
 				new_link_timer(nodeID);
 				e.printStackTrace();
 			}
