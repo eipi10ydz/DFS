bbba4467bc3a7ecf54fc92762b92c9867ad8ef7f
王正陽 <wzy9607@mail.ustc.edu.cn>
2016-06-18 03:25:57 +0000

    Replace DataReceiver.java

--- src/nat/node/DataReceiver.java
+++ src/nat/node/DataReceiver.java
@@ -1,9 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package nodetest;
+package data_transferor;
 
 import com.barchart.udt.ExceptionUDT;
 import com.barchart.udt.SocketUDT;
@@ -21,76 +16,73 @@ import java.util.logging.Logger;
 
 /**
  *
- * @author Êù®Âæ∑‰∏≠
+ * @author —Óµ¬÷–
  */
-public class DataReceiver implements Runnable
-{
-    protected String from;
-    protected int cnt;
-    protected int No;
-    protected String res;
-    protected Map<Integer, String> pack;
-    protected Timer timer;
-    protected Node node;
-    
-    public DataReceiver(String from, int cnt, int No, Node node)
-    {
-        this.from = from;
-        this.cnt = cnt;
-        this.No = No;
-        this.res = "";
-        this.pack = new ConcurrentHashMap<>();
-        this.timer = new Timer(300000); //5ÂàÜÈíü?
-    }
-    
-    /**
-     *
-     */
-    @Override
-    public void run()
-    {
-        while(this.pack.size() < this.cnt && (!this.timer.isExpired()))
-            ;
-        if(this.pack.size() < this.cnt)
-        {
-            Map<String, String> info = new HashMap<>();
-            info.put("ID", from);
-            info.put("ID_target", node.ID);
-            info.put("No", No + "");
-            SocketUDT sock;
-            try {
-                sock = new SocketUDT(TypeUDT.STREAM);
-                sock.connect(new InetSocketAddress(node.server_host, node.server_port));
-                sock.send(Packer.pack("DataR", info).getBytes(Charset.forName("ISO-8859-1")));
-            } 
-            catch (ExceptionUDT | PackException ex) 
-            {
-                Logger.getLogger(DataReceiver.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            return;
-        }
-        for(int i = 0; i < this.cnt; ++i)
-        {
-            res = res + this.pack.get(i);
-        }
-        ObjectInputStream oin = null;
-        Object obj = null;
-        try 
-        {
-            //ÂæóÂà∞ÂÆåÊï¥ÂÜÖÂÆπ
-            oin = new ObjectInputStream(new ByteArrayInputStream(res.getBytes(Charset.forName("ISO-8859-1"))));
-            obj = oin.readObject();
-        } 
-        catch (IOException ex) 
-        {
-//            Logger.getLogger(DataReceiver.class.getName()).log(Level.SEVERE, null, ex);
-            System.out.println("get object failed...");
-        } 
-        catch (ClassNotFoundException ex) 
-        {
-            Logger.getLogger(DataReceiver.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        System.out.println(obj.toString());
-    }
+public class DataReceiver implements Runnable {
+	protected String from;
+	protected int cnt;
+	protected int No;
+	protected String res;
+	protected Map<Integer, String> pack;
+	protected Timer timer;
+	protected Node node;
+
+	/**
+	 * @param from
+	 * @param cnt
+	 * @param No
+	 * @param node
+	 */
+	public DataReceiver(String from, int cnt, int No, Node node) {
+		this.from = from;
+		this.cnt = cnt;
+		this.No = No;
+		this.res = "";
+		this.pack = new ConcurrentHashMap<>();
+		this.timer = new Timer(300000); // 5∑÷÷”?
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Runnable#run()
+	 */
+	@Override
+	public void run() {
+		while (this.pack.size() < this.cnt && (!this.timer.isExpired()))
+			;
+		if (this.pack.size() < this.cnt) {
+			Map<String, String> info = new HashMap<>();
+			info.put("ID", from);
+			info.put("ID_target", node.ID);
+			info.put("No", No + "");
+			SocketUDT sock;
+			try {
+				sock = new SocketUDT(TypeUDT.STREAM);
+				sock.connect(new InetSocketAddress(node.server_host, node.server_port));
+				sock.send(Packer.pack("DataR", info).getBytes(Charset.forName("ISO-8859-1")));
+			} catch (ExceptionUDT | PackException ex) {
+				Logger.getLogger(DataReceiver.class.getName()).log(Level.SEVERE, null, ex);
+			}
+			return;
+		}
+		for (int i = 0; i < this.cnt; ++i) {
+			res = res + this.pack.get(i);
+		}
+		ObjectInputStream oin = null;
+		Object obj = null;
+		try {
+			// µ√µΩÕÍ’˚ƒ⁄»›
+			oin = new ObjectInputStream(new ByteArrayInputStream(res.getBytes(Charset.forName("ISO-8859-1"))));
+			obj = oin.readObject();
+		} catch (IOException ex) {
+			// Logger.getLogger(DataReceiver.class.getName()).log(Level.SEVERE,
+			// null, ex);
+			System.out.println("get object failed...");
+		} catch (ClassNotFoundException ex) {
+			Logger.getLogger(DataReceiver.class.getName()).log(Level.SEVERE, null, ex);
+		}
+		System.out.println(obj.toString());
+	}
 
 }
